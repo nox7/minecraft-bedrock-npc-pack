4d8ada16bcb3d37e4ba806c9a25c867c2572ed4f
Cole Green <17110935+nox7@users.noreply.github.com>
2025-02-17 15:23:21 -0600

    Add missing PathfindingSuite

---
+++ scripts/PathfindingSuite/Pathfinder/AStar.ts
@@ -0,0 +1,321 @@
+import { Block, Vector3, system } from "@minecraft/server";
+import { CuboidRegion } from "../Region/CuboidRegion";
+import { AStarOptions } from "./AStarOptions";
+import { BlockSafetyCheckerUtility } from "../BlockSafetyChecker/BlockSafetyCheckerUtility";
+import { BlockSafetyCheckerOptions } from "../BlockSafetyChecker/BlockSafetyCheckerOptions";
+import { BlockSafetyCheckResult } from "../BlockSafetyChecker/BlockSafetyCheckResult";
+import { IAStarNode } from "./Interfaces/IAStarNode";
+import { ClosedAStarLocations } from "./Types/ClosedAStarLocations";
+import { MinecraftBlockTypes } from "@minecraft/vanilla-data";
+import { Vector3Utils } from "@minecraft/math";
+
+/**
+ * Implementation of the A* algorithm for Minecraft
+ */
+export class AStar{
+
+    private Options: AStarOptions;
+    private StartBlock: Block;
+    private EndBlock: Block;
+    private TotalDebugBlocksCreated: Set<Block>;
+
+    public constructor(options: AStarOptions){
+        this.Options = options;
+        this.TotalDebugBlocksCreated = new Set();
+
+        let startBlock: Block | undefined;
+        let endBlock: Block | undefined;
+        try{
+            startBlock = this.Options.Dimension.getBlock(this.Options.StartLocation);
+            endBlock = this.Options.Dimension.getBlock(this.Options.GoalLocation);
+        }catch(e){
+        }
+        if (startBlock !== undefined && endBlock !== undefined){
+            this.StartBlock = startBlock;
+            this.EndBlock = endBlock;
+        }else{
+            throw "Start and End must point to valid and loaded blocks.";
+        }
+        
+    }
+
+    /**
+     * Initiates A* pathfinding to the GoalLocation provided in the constructor's AStarOptions. Throws an exception if a path cannot be found.
+     * @throws
+     */
+    public async Pathfind(): Promise<Block[]>{
+        // Iterate over possible nodes
+        // If the iterated node is the goal location, then we're done
+        // for
+        const goalNode = await new Promise<IAStarNode>(goalNodeResolve => {
+            system.runJob(this.IterateUntilGoalBlockReached(goalNodeResolve));
+        });
+
+        const blockPath: Block[] = [];
+        let currentNode: IAStarNode | null = goalNode;
+        while (currentNode !== null){
+            blockPath.push(currentNode.Block);
+            currentNode = currentNode.ParentNode;
+        }
+        this.ClearDebugBlocks();
+        return blockPath.reverse();
+    }
+
+    /**
+     * Used in a system.runJob call. Used to iterate over all nodes in an A* pathfinding algorithm. Yields often and returns (finishes) by calling the goalNodePromiseResolve to finish the generator. Throws exceptions if a path cannot be found.
+     * @throws
+     */
+    public *IterateUntilGoalBlockReached(goalNodePromiseResolve: (goalNode: IAStarNode) => void){
+        const openList: IAStarNode[] = [ 
+            {
+                Block: this.StartBlock, 
+                ParentNode: null,
+                FCost: this.CalculateFCost(this.StartBlock),
+                GCost: this.CalculateGCost(this.StartBlock),
+                HCost: this.CalculateHHeuristic(this.StartBlock),
+            }
+        ];
+
+        // Keep track of locations we've closed and won't consider again
+        const closedListLocations: ClosedAStarLocations = {};
+
+        // Iterate until the openList is exhausted of all options
+        while (openList.length > 0){
+            // Check if we have considered too many nodes and the path may be too difficult or impossible to get to
+            if (Object.keys(closedListLocations).length >= this.Options.MaximumNodesToConsider){
+                this.ClearDebugBlocks();
+                throw "Maximum number of nodes considered. MaximumNodesToConsider limit option hit.";
+            }
+
+            // Find the next node in the openList with the lowest F cost
+            const nextIndex: number = this.GetIndexOfNodeWithLowestFCost(openList);
+            const nextNode: IAStarNode = openList[nextIndex];
+            const locationHash: string = Vector3Utils.toString(nextNode.Block.location);
+
+            // Check if the nextNode is the EndBlock
+            if (Vector3Utils.equals(nextNode.Block, this.EndBlock)){
+                return goalNodePromiseResolve(nextNode);
+            }
+
+            // Remove the nextIndex from the open node list
+            openList.splice(nextIndex, 1);
+
+            // Add it to closed list locations
+            closedListLocations[locationHash] = nextNode;
+
+            // Get all the adjacent locations surrounding the nextNode.Block
+            let surroundingLocations: Vector3[];
+            if(!this.Options.AllowYAxisFlood) {
+                surroundingLocations = CuboidRegion.FromCenterLocation(nextNode.Block.location, 1, true).GetAllLocationsInRegion();
+            } else {
+                surroundingLocations = CuboidRegion.GetAdjacentPositions(nextNode.Block.location, 1);
+            }
+            const surroundingBlocks: Block[] = [];
+
+            // Load block safety options
+            const safetyCheckOptions = new BlockSafetyCheckerOptions();
+            safetyCheckOptions.TagsToConsiderPassable = this.Options.TagsToConsiderPassable;
+            safetyCheckOptions.TypeIdsToConsiderPassable = this.Options.TypeIdsToConsiderPassable;
+            safetyCheckOptions.TypeIdsThatCannotBeJumpedOver = this.Options.TypeIdsThatCannotBeJumpedOver;
+            safetyCheckOptions.AllowYAxisFlood = this.Options.AllowYAxisFlood;
+
+            for (const location of surroundingLocations){
+                let blockAtLocation: Block | undefined;
+                try{
+                    blockAtLocation = this.Options.Dimension.getBlock(location);
+                }catch(e){}
+
+                if (blockAtLocation !== undefined && blockAtLocation.isValid){
+                    // Is it the block we're looking for?
+                    if (Vector3Utils.equals(location, this.Options.GoalLocation)){
+                        // Add it regardless of safety
+                        surroundingBlocks.push(blockAtLocation);
+                    }else{
+                        // Check it is safe to move to, fall down from, or jump ontop of
+                        const safetyCheckResult: BlockSafetyCheckResult = BlockSafetyCheckerUtility.RunBlockSafetyCheck(blockAtLocation, safetyCheckOptions);
+
+                        if (safetyCheckResult.IsSafe){
+                            if(safetyCheckOptions.AllowYAxisFlood) {
+                                const belowBlock = blockAtLocation.below();
+                                const upBlock = blockAtLocation.above();
+                                const bottomSafetyCheckResult = BlockSafetyCheckerUtility.RunBlockSafetyCheck(belowBlock!, safetyCheckOptions);
+                                const topSafetyCheckResult = BlockSafetyCheckerUtility.RunBlockSafetyCheck(upBlock!, safetyCheckOptions);
+                                if(bottomSafetyCheckResult.IsSafe) surroundingBlocks.push(belowBlock!);
+                                if(topSafetyCheckResult.IsSafe) surroundingBlocks.push(upBlock!);
+                                surroundingBlocks.push(blockAtLocation);
+                                continue;
+                            }
+                            // Check if it's safe to fall from
+                            if (safetyCheckResult.CanSafelyFallFrom){
+                                // Use the block below blockAtLocation
+                                surroundingBlocks.push(<Block>blockAtLocation.below(1));
+                            }else if (safetyCheckResult.CanSafelyJumpOnto){
+                                // Use the block above blockAtLocation
+                                surroundingBlocks.push(<Block>blockAtLocation.above(1));
+                            }else{
+                                // The 'blockAtLocation' itself is fine
+                                surroundingBlocks.push(blockAtLocation);
+                            }
+                        }
+                    }
+                }
+                yield;
+            }
+
+            // The surroundingBlocks array is now an array of blocks that can be walked to, jumped to, or fallen to - all safely
+            for (const surroundingBlock of surroundingBlocks){
+                // Build the surrounding node from the surroundingBlock and set the parent as the current nextNode
+                const surroundingNode: IAStarNode = {
+                    Block: surroundingBlock, 
+                    ParentNode: nextNode,
+                    FCost: this.CalculateFCost(surroundingBlock),
+                    GCost: nextNode.GCost + 1,// this.CalculateGCost(surroundingBlock),
+                    HCost: this.CalculateHHeuristic(surroundingBlock),
+                };
+                const surroundingBlockLocationHash: string = Vector3Utils.toString(surroundingBlock.location);
+
+                
+                // Check if this block is the end block
+                if (Vector3Utils.equals(surroundingBlock, this.EndBlock)){
+                    return goalNodePromiseResolve(surroundingNode);
+                }
+                
+                // Check if the block is in the closed list
+                if (surroundingBlockLocationHash in closedListLocations){
+                    // Skip this block
+                    continue;
+                }
+                // Creates a path traces using Structure Void whenever in debug mode
+                this.SetDebugBlock(surroundingBlock);
+
+                // Check if the block is already in the open list
+                const indexOfExistingNodeInOpenList: number | null = this.GetIndexOfNodeIfInList(surroundingNode, openList);
+                if (indexOfExistingNodeInOpenList === null){
+                    // It is not in the openList, add it to the open list
+                    openList.push(surroundingNode);
+                }else{
+                    // It's already in the openList
+                    // Compare the existing openList index node value to the surroundingNode g cost value
+                    // If the surroundingNode.gCost is less than the existing openList index node value, then
+                    // modify the existing openLiset index node value to have the new gCost and fCost and parent to match surroundingNode's properties
+                    if (openList[indexOfExistingNodeInOpenList].GCost > surroundingNode.GCost){
+                        openList[indexOfExistingNodeInOpenList].GCost = surroundingNode.GCost;
+                        openList[indexOfExistingNodeInOpenList].ParentNode = surroundingNode.ParentNode;
+                    }
+                }
+
+                yield;
+            }
+        }
+
+        // Out of options if we get here without returning
+        this.ClearDebugBlocks();
+        throw "No path could be found to the destination. All adjacent moveable nodes to consider has been exhausted.";
+    }
+
+    /**
+     * Sets a debug block (structure void) to visualize how the pathfinding traverses considering the passable blocks.
+     * @param block The block to put the debug blocks in.
+     */
+    private SetDebugBlock(block: Block) {
+        if(!this.Options.DebugMode) return;
+        if(!(Vector3Utils.toString(block.location) 
+            in 
+            [...this.TotalDebugBlocksCreated].map((b: Block)=> Vector3Utils.toString(b.location)))
+        ) this.TotalDebugBlocksCreated.add(block);
+        this.Options.Dimension.setBlockType(block, MinecraftBlockTypes.StructureVoid);
+    }
+
+    /**
+     * Clears all the placed debug blocks from the pathfinding execution.
+     * @returns 
+     */
+    private ClearDebugBlocks() {
+        if(!this.Options.DebugMode) return;
+        const locs = this.TotalDebugBlocksCreated;
+        const safetyCheckOptions = new BlockSafetyCheckerOptions();
+        safetyCheckOptions.TagsToConsiderPassable = this.Options.TagsToConsiderPassable;
+        safetyCheckOptions.TypeIdsToConsiderPassable = this.Options.TypeIdsToConsiderPassable;
+        safetyCheckOptions.TypeIdsThatCannotBeJumpedOver = this.Options.TypeIdsThatCannotBeJumpedOver;
+        safetyCheckOptions.AllowYAxisFlood = this.Options.AllowYAxisFlood;
+        for(const v of locs){
+            system.run(() => {
+                const vx = v.location;
+                if(Vector3Utils.equals(vx, this.Options.StartLocation) || Vector3Utils.equals(vx, this.Options.GoalLocation)) return
+                if(!v.matches(MinecraftBlockTypes.StructureVoid)) return;
+                this.Options.Dimension.setBlockType(vx, MinecraftBlockTypes.Air);
+            });
+        }
+    }
+
+    /**
+     * Checks if a node is present in the provided array of nodes by checking if the provided node's block matches any block locations 
+     * in the listOfNodes array.
+     * @param listOfBlocks
+     * @param block 
+     * @returns 
+     */
+    private GetIndexOfNodeIfInList(node: IAStarNode, listOfNodes: IAStarNode[]): number | null{
+        for (const index in listOfNodes){
+            const indexNumber: number = parseInt(index);
+            const nodeInList: IAStarNode = listOfNodes[indexNumber];
+            if (Vector3Utils.equals(node.Block, nodeInList.Block)){
+                return indexNumber;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the index of the block with the lowest calculated F cost from a list of nodes
+     * @param listOfNodes 
+     * @returns 
+     */
+    private GetIndexOfNodeWithLowestFCost(listOfNodes: IAStarNode[]): number{
+        let currentLowestIndex = -1;
+        let currentLowestFCost = -1;
+
+        for (const index in listOfNodes){
+            let indexNumber: number = parseInt(index);
+            if (currentLowestIndex === -1){
+                currentLowestFCost = this.CalculateFCost(listOfNodes[indexNumber].Block);
+                currentLowestIndex = indexNumber;
+            }else{
+                const thisFCost = this.CalculateFCost(listOfNodes[indexNumber].Block);
+                if (thisFCost < currentLowestFCost){
+                    currentLowestFCost = thisFCost;
+                    currentLowestIndex = indexNumber;
+                }
+            }
+        }
+
+        return currentLowestIndex;
+    }
+
+    /**
+     * Calculates the "F" cost of a node, which is F = G + H
+     * @param Block
+     */
+    private CalculateFCost(block: Block): number{
+        return this.CalculateGCost(block) + this.CalculateHHeuristic(block);
+    }
+
+    /**
+     * Calculates the "G" cost of a node
+     * @param block 
+     * @returns 
+     */
+    private CalculateGCost(block: Block): number{
+        return Vector3Utils.distance(block.location, this.Options.StartLocation);
+    }
+
+    /**
+     * Calculates the "H" heuristic of a node
+     * @param block 
+     * @returns 
+     */
+    private CalculateHHeuristic(block: Block): number{
+        return Vector3Utils.distance(block.location, this.Options.GoalLocation);
+    }
+}
---
+++ scripts/PathfindingSuite/Region/CuboidRegion.ts
@@ -0,0 +1,195 @@
+import { Vector3 } from "@minecraft/server";
+
+export class CuboidRegion{
+    public Corner1: Vector3;
+    public Corner2: Vector3;
+    public CuboidRadius: number;
+    public IsVerticallyFlat: boolean;
+
+    public static FromCenterLocation(location: Vector3, cuboidRadius: number, isVerticallyFlat: boolean){
+        cuboidRadius = Math.round(cuboidRadius);
+        const corner1: Vector3 = {
+            x: location.x - cuboidRadius,
+            y: (!isVerticallyFlat ? (location.y - cuboidRadius) : location.y),
+            z: location.z - cuboidRadius,
+        };
+        const corner2: Vector3 = {
+            x: location.x + cuboidRadius,
+            y: (!isVerticallyFlat ? (location.y + cuboidRadius) : location.y),
+            z: location.z + cuboidRadius,
+        }
+        return new CuboidRegion(corner1, corner2, cuboidRadius, isVerticallyFlat);
+    }
+
+    public static GetAdjacentPositions(centerLocation: Vector3, cuboidRadius: number): Vector3[] {
+        const positions: Vector3[] = [];
+        
+        const directions = [
+            { x: 0, y: 1, z: 0 },  // Up
+            { x: 0, y: -1, z: 0 }, // Down
+            { x: 1, y: 0, z: 0 },  // Right
+            { x: -1, y: 0, z: 0 }, // Left
+            { x: 0, y: 0, z: 1 },  // Forward
+            { x: 0, y: 0, z: -1 }  // Backward
+        ];
+    
+        for (const direction of directions) {
+            positions.push({
+                x: centerLocation.x + direction.x * cuboidRadius,
+                y: centerLocation.y + direction.y * cuboidRadius,
+                z: centerLocation.z + direction.z * cuboidRadius,
+            });
+        }
+    
+        return positions;
+    }
+    
+
+    /**
+     * Gets a list of Vector3s that lie on the outer edge of a cube at the centerLocation with the provided cuboidRadius
+     * @param centerLocation
+     * @param cuboidRadius 
+     */
+    public static GetPositionsAlongOuterEdgeOfCube(centerLocation: Vector3, cuboidRadius: number){
+        const existingPositionsHashMap: {[key: string]: boolean} = {};
+        const positions: Vector3[] = [];
+        // Get left side of cube
+        for (let x = -cuboidRadius; x == -cuboidRadius; x++){
+            for (let y = -cuboidRadius; y <= cuboidRadius; y++){
+                for (let z = -cuboidRadius; z <= cuboidRadius; z++){
+                    const hash = `${x},${y},${z}`;
+                    if (!(hash in existingPositionsHashMap)){
+                        positions.push({
+                            x: centerLocation.x + x,
+                            y: centerLocation.y + y,
+                            z: centerLocation.z + z,
+                        });
+                        existingPositionsHashMap[hash] = true;
+                    }
+                }
+            }
+        }
+
+        // Get ride side of cube
+        for (let x = cuboidRadius; x == cuboidRadius; x++){
+            for (let y = -cuboidRadius; y <= cuboidRadius; y++){
+                for (let z = -cuboidRadius; z <= cuboidRadius; z++){
+                    const hash = `${x},${y},${z}`;
+                    if (!(hash in existingPositionsHashMap)){
+                        positions.push({
+                            x: centerLocation.x + x,
+                            y: centerLocation.y + y,
+                            z: centerLocation.z + z,
+                        });
+                        existingPositionsHashMap[hash] = true;
+                    }
+                }
+            }
+        }
+
+        // Get top side of cube
+        for (let y = cuboidRadius; y == cuboidRadius; y++){
+            for (let x = -cuboidRadius; x <= cuboidRadius; x++){
+                for (let z = -cuboidRadius; z <= cuboidRadius; z++){
+                    const hash = `${x},${y},${z}`;
+                    if (!(hash in existingPositionsHashMap)){
+                        positions.push({
+                            x: centerLocation.x + x,
+                            y: centerLocation.y + y,
+                            z: centerLocation.z + z,
+                        });
+                        existingPositionsHashMap[hash] = true;
+                    }
+                }
+            }
+        }
+
+        // Get bottom side of cube
+        for (let y = -cuboidRadius; y == -cuboidRadius; y++){
+            for (let x = -cuboidRadius; x <= cuboidRadius; x++){
+                for (let z = -cuboidRadius; z <= cuboidRadius; z++){
+                    const hash = `${x},${y},${z}`;
+                    if (!(hash in existingPositionsHashMap)){
+                        positions.push({
+                            x: centerLocation.x + x,
+                            y: centerLocation.y + y,
+                            z: centerLocation.z + z,
+                        });
+                        existingPositionsHashMap[hash] = true;
+                    }
+                }
+            }
+        }
+
+        // Get front side of cube
+        for (let z = cuboidRadius; z == cuboidRadius; z++){
+            for (let x = -cuboidRadius; x <= cuboidRadius; x++){
+                for (let y = -cuboidRadius; y <= cuboidRadius; y++){
+                    const hash = `${x},${y},${z}`;
+                    if (!(hash in existingPositionsHashMap)){
+                        positions.push({
+                            x: centerLocation.x + x,
+                            y: centerLocation.y + y,
+                            z: centerLocation.z + z,
+                        });
+                        existingPositionsHashMap[hash] = true;
+                    }
+                }
+            }
+        }
+
+        // Get back side of cube
+        for (let z = -cuboidRadius; z == -cuboidRadius; z++){
+            for (let x = -cuboidRadius; x <= cuboidRadius; x++){
+                for (let y = -cuboidRadius; y <= cuboidRadius; y++){
+                    const hash = `${x},${y},${z}`;
+                    if (!(hash in existingPositionsHashMap)){
+                        positions.push({
+                            x: centerLocation.x + x,
+                            y: centerLocation.y + y,
+                            z: centerLocation.z + z,
+                        });
+                        existingPositionsHashMap[hash] = true;
+                    }
+                }
+            }
+        }
+
+        return positions;
+    }
+
+    public constructor(corner1: Vector3, corner2: Vector3, cuboidRadius: number, isVerticallyFlat: boolean){
+        this.Corner1 = corner1;
+        this.Corner2 = corner2;
+        this.CuboidRadius = cuboidRadius;
+        this.IsVerticallyFlat = isVerticallyFlat;
+    }
+
+    /**
+     * Gets all the Vector3 locations that are contained within this region
+     */
+    public GetAllLocationsInRegion(): Vector3[]{
+        const locations: Vector3[] = [];
+        const top: Vector3 = {
+            x: Math.min(this.Corner1.x, this.Corner2.x),
+            y: Math.min(this.Corner1.y, this.Corner2.y),
+            z: Math.min(this.Corner1.z, this.Corner2.z),
+        };
+
+        const bottom: Vector3 = {
+            x: Math.max(this.Corner1.x, this.Corner2.x),
+            y: Math.max(this.Corner1.y, this.Corner2.y),
+            z: Math.max(this.Corner1.z, this.Corner2.z),
+        };
+
+        for (let x = top.x; x <= bottom.x; x++){
+            for (let y = top.y; y <= bottom.y; y++){
+                for (let z = top.z; z <= bottom.z; z++){
+                    locations.push({x: x, y: y, z :z});
+                }
+            }
+        }
+
+        return locations;
+    }
+}
